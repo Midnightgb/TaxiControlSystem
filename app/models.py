3a4dbf559236db61bdc19abd7cb8e2bc7075ad84
Midnightgb <midnight3424@gmail.com>
2023-11-07 00:09:24 -0500

    Add Pago and Reporte models to database

--- app/models.py
+++ app/models.py
@@ -5,13 +5,16 @@ from enum import Enum as PyEnum
 
 Base = declarative_base()
 
+
 class Rol(PyEnum):
     Administrador = "Administrador"
     Conductor = "Conductor"
 
+
 class Estado(PyEnum):
     Activo = "Activo"
     Inactivo = "Inactivo"
+
 
 class Usuario(Base):
     __tablename__ = "usuarios"
@@ -25,7 +28,9 @@ class Usuario(Base):
     rol = Column(Enum(Rol), nullable=False)
     estado = Column(Enum(Estado), nullable=False)
     created_at = Column(String, server_default=func.now(), nullable=False)
-    updated_at = Column(String, server_default=func.now(), onupdate=func.now(), nullable=False)
+    updated_at = Column(String, server_default=func.now(),
+                        onupdate=func.now(), nullable=False)
+
 
 class Taxi(Base):
     __tablename__ = "taxis"
@@ -37,9 +42,11 @@ class Taxi(Base):
     marca = Column(String(45), nullable=False)
     fecha_adquisicion = Column(Date)
     created_at = Column(String, server_default=func.now(), nullable=False)
-    updated_at = Column(String, server_default=func.now(), onupdate=func.now(), nullable=False)
+    updated_at = Column(String, server_default=func.now(),
+                        onupdate=func.now(), nullable=False)
 
     conductor = relationship("Usuario", back_populates="taxis")
+
 
 class Mantenimiento(Base):
     __tablename__ = "mantenimientos"
@@ -50,77 +57,36 @@ class Mantenimiento(Base):
     descripcion = Column(String(155), nullable=False)
     costo = Column(Integer, nullable=False)
     created_at = Column(String, server_default=func.now(), nullable=False)
-    updated_at = Column(String, server_default=func.now(), onupdate=func.now(), nullable=False)
+    updated_at = Column(String, server_default=func.now(),
+                        onupdate=func.now(), nullable=False)
 
     taxi = relationship("Taxi", back_populates="mantenimientos")
 
 
+class Pago(Base):
+    __tablename__ = "pagos"
+
+    id_pago = Column(Integer, primary_key=True, autoincrement=True)
+    id_conductor = Column(Integer, ForeignKey("usuarios.id_usuario"))
+    fecha = Column(Date, nullable=False)
+    valor = Column(Integer, nullable=False)
+    created_at = Column(String, server_default=func.now(), nullable=False)
+    updated_at = Column(String, server_default=func.now(),
+                        onupdate=func.now(), nullable=False)
+
+    conductor = relationship("Usuario", back_populates="pagos")
 
 
+class Reporte(Base):
+    __tablename__ = "reportes"
 
-""" 
-CREATE DATABASE IF NOT EXISTS `taxicontrolsystem`;
-USE `taxicontrolsystem`;
+    id_reporte = Column(Integer, primary_key=True, autoincrement=True)
+    id_conductor = Column(Integer, ForeignKey("usuarios.id_usuario"))
+    fecha = Column(Date, nullable=False)
+    ingresos = Column(Integer, nullable=False)
+    gastos = Column(Integer, nullable=False)
+    created_at = Column(String, server_default=func.now(), nullable=False)
+    updated_at = Column(String, server_default=func.now(),
+                        onupdate=func.now(), nullable=False)
 
-CREATE TABLE `usuarios` (
-  `id_usuario` INT NOT NULL AUTO_INCREMENT,
-  `cedula` INT NOT NULL UNIQUE,
-  `nombre` VARCHAR(45) NOT NULL,
-  `apellido` VARCHAR(45) NOT NULL,
-  `correo` VARCHAR(45),
-  `contrasena` VARCHAR(45),
-  `rol` enum('Administrador','Conductor') NOT NULL,
-  `estado` enum('Activo','Inactivo') DEFAULT 'Activo' NOT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-CREATE TABLE `taxis` (
-  `id_taxi` INT NOT NULL AUTO_INCREMENT,
-  `id_conductor` INT,
-  `placa` VARCHAR(6) NOT NULL UNIQUE,
-  `modelo` VARCHAR(45) NOT NULL,
-  `marca` VARCHAR(45) NOT NULL,
-  `fecha_adquisicion` DATE,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_taxi`),
-  FOREIGN KEY (`id_conductor`) REFERENCES `usuarios`(`id_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-CREATE TABLE `mantenimientos` (
-  `id_mantenimiento` INT NOT NULL AUTO_INCREMENT,
-  `id_taxi` INT NOT NULL,
-  `fecha` DATE NOT NULL,
-  `descripcion` VARCHAR(155) NOT NULL,
-  `costo` INT NOT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_mantenimiento`),
-  FOREIGN KEY (`id_taxi`) REFERENCES `taxis`(`id_taxi`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;  
-
-CREATE TABLE `pagos` (
-  `id_pago` INT NOT NULL AUTO_INCREMENT,
-  `id_conductor` INT NOT NULL,
-  `fecha` DATE NOT NULL,
-  `valor` INT NOT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_pago`),
-  FOREIGN KEY (`id_conductor`) REFERENCES `usuarios`(`id_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-CREATE TABLE `reportes` (
-  `id_reporte` INT NOT NULL AUTO_INCREMENT,
-  `id_conductor` INT NOT NULL,
-  `fecha` DATE NOT NULL,
-  `ingresos` INT NOT NULL,
-  `gastos` INT NOT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_reporte`),
-  FOREIGN KEY (`id_conductor`) REFERENCES `usuarios`(`id_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
- """
+    conductor = relationship("Usuario", back_populates="reportes")
