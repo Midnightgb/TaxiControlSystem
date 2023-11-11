3940ce9fd5608768c449464cb2170ebe1d8de4bb
Midnightgb <midnight3424@gmail.com>
2023-11-11 15:38:48 -0500

    Add login functionality with password encryption

--- app/main.py
+++ app/main.py
@@ -14,6 +14,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session, joinedload
+import bcrypt
 
 from models import Usuario
 from database import get_database
@@ -26,15 +27,63 @@ app.mount("/static", StaticFiles(directory="public/dist"), name="static")
 templates = Jinja2Templates(directory="public/templates")
 
 
-@app.get("/", response_class=HTMLResponse, tags=["login"])
-async def root(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
+@app.get("/", tags=["routes"])
+async def root():
+    return RedirectResponse(url="/login")
 
-@app.get("/login/recover", response_class=HTMLResponse, tags=["login"])
+
+@app.get("/login", response_class=HTMLResponse, tags=["routes"])
+async def login(request: Request, alert: str = None):
+    print("alerta")
+    print(alert)
+    return templates.TemplateResponse("./auth/login.html", {"request": request, "alert": alert})
+
+
+@app.get("/login/recover", response_class=HTMLResponse, tags=["routes"])
 async def recover(request: Request):
-    return templates.TemplateResponse("recover.html", {"request": request})
-# -- PATH TO REDIRECT TO USER CREATION -- #
+    return templates.TemplateResponse("./auth/recover.html", {"request": request})
 
+@app.post("/login", response_class=HTMLResponse, tags=["auth"])
+async def login_post(
+    request: Request,
+    user: Optional[str] = Form(None),
+    contrasena: Optional[str] = Form(None),
+    db: Session = Depends(get_database)
+):
+    if not all([user, contrasena]):
+        alert = {"tipo": "danger", "mensaje": "Todos los campos son requeridos", "class" : "error"}
+        return RedirectResponse(url=f"/login?alert={alert}", status_code=status.HTTP_303_SEE_OTHER)
+    
+    if not user:
+        alert = {"tipo": "danger", "mensaje": "El usuario es requerido", "class" : "error"}
+        return RedirectResponse(url=f"/login?alert={alert}", status_code=status.HTTP_303_SEE_OTHER)
+    if not contrasena:
+        alert = {"tipo": "danger", "mensaje": "La contraseña es requerida", "class" : "error"}
+        return RedirectResponse(url=f"/login?alert={alert}", status_code=status.HTTP_303_SEE_OTHER)
+    
+    if '@' in user:
+        usuario = db.query(Usuario).filter(Usuario.correo == user).first()
+    else:
+        usuario = db.query(Usuario).filter(Usuario.cedula == user).first()
+
+    if not usuario:
+        alert = {"tipo": "danger", "mensaje": "Usuario incorrecto", "class" : "error"}
+        return RedirectResponse(url=f"/login?alert={alert}", status_code=status.HTTP_303_SEE_OTHER)
+    
+    if not bcrypt.checkpw(contrasena.encode('utf-8'), usuario.contrasena.encode('utf-8')):
+        alert = {"tipo": "danger", "mensaje": "Contraseña incorrecta", "class" : "error"}
+        return RedirectResponse(url=f"/login?alert={alert}", status_code=status.HTTP_303_SEE_OTHER)
+
+    if usuario.estado != 'Activo':
+        alert = {"tipo": "danger", "mensaje": "Usuario inactivo", "class" : "error"}
+        return RedirectResponse(url=f"/login?alert={alert}", status_code=status.HTTP_303_SEE_OTHER)
+    
+    #response = RedirectResponse(url="/home", status_code=status.HTTP_303_SEE_OTHER)
+
+@app.get("/home", response_class=HTMLResponse, tags=["routes"])
+async def home(request: Request):
+    return templates.TemplateResponse("./index.html", {"request": request})
+# -- PATH TO REDIRECT TO USER CREATION -- #
 
 @app.get("/PathCreateUser", response_class=HTMLResponse, tags=["create"])
 async def create(request: Request):
@@ -74,3 +123,4 @@ async def CreateUser(
     db.refresh(nuevo_usuario)
 
     return templates.TemplateResponse("index.html", {"request": request})
+
