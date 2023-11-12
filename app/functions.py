8317348d57ec1e83a65ae3b38cf6499f6527e489
Midnightgb <midnight3424@gmail.com>
2023-11-11 21:03:24 -0500

    Update requirements.txt and login.css, add python-dotenv import and adjust form height, login working

--- app/functions.py
+++ app/functions.py
@@ -1,11 +1,15 @@
 from jose import jwt
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-def tokenConstructor(usuario_id: str):
-    expiration = datetime.utcnow() + timedelta(hours=1)  
-    payload = {"sub": usuario_id, "exp": expiration}
+
+def tokenConstructor(userId: str):
+    print("##########$$$$$$$$$$$$########## tokenConstructor ##########$$$$$$$$$$$$##########")
+    expiration = datetime.utcnow() + timedelta(hours=1)
+    payload = {"sub": userId, "exp": expiration}
     token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
     return token
