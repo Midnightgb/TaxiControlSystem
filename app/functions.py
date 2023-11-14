fd2457428a76f1ca55a5d83d6e437693e19e2c04
Midnightgb <midnight3424@gmail.com>
2023-11-13 19:50:01 -0500

    Increase token expiration time to 1 hour.

--- app/functions.py
+++ app/functions.py
@@ -19,7 +19,7 @@ load_dotenv()
 SECRET_KEY = os.environ.get("SECRET_KEY")
 def tokenConstructor(userId: str):
     print("##########$$$$$$$$$$$$########## tokenConstructor ##########$$$$$$$$$$$$##########")
-    expiration = datetime.utcnow() + timedelta(seconds=20)
+    expiration = datetime.utcnow() + timedelta(hours=1)
     payload = {"sub": str(userId), "exp": expiration}
     token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
     return token
