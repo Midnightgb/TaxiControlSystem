76944573e03806ea214d9823c341b63523775cdc
riohp <carloshenao0418@gmail.com>
2023-12-03 22:26:12 -0500

    Update payment.js and dailyreports.html

--- app/public/dist/js/payment.js
+++ app/public/dist/js/payment.js
@@ -2,7 +2,7 @@ function openUpdatePaymentModal(idConductor, idPago, valorActual) {
     document.getElementById('updatePaymentModal').classList.remove('hidden');
     document.getElementById('id_conductor').value = idConductor;
     document.getElementById('id_pago').value = idPago;
-    document.getElementById('valor').value = valorActual;
+    document.getElementById('valorpagos').value = valorActual;
 }
 
 function closeUpdatePaymentModal() {
