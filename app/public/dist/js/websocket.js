1d3aa3a641e7c8501c974ae176f5b8f62fc5354f
midnightgb <midnight3424@gmail.com>
2023-12-04 07:45:17 -0500

    Fix WebSocket URL in pr.html

---
+++ app/public/dist/js/websocket.js
@@ -0,0 +1,21 @@
+console.log("websocket.js loaded");
+  console.log("window loaded");
+var client_id = Date.now()
+//document.querySelector("#ws-id").textContent = client_id;
+console.log("client_id: ", client_id);
+var ws = new WebSocket(`wss://6935-191-156-47-50.ngrok-free.app/ws/${client_id}`);
+ws.onmessage = function(event) {
+    var messages = document.getElementById('messages')
+    var message = document.createElement('li')
+    var content = document.createTextNode(event.data)
+    message.appendChild(content)
+    messages.appendChild(message)
+};
+
+function sendMessage(event) {
+    var input = document.getElementById("messageText")
+    ws.send(input.value)
+    input.value = ''
+    event.preventDefault()
+}
+
